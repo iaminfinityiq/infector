af37aa17f20c05b85661603bad90c7f789ac2315
iaminfinityiq <133767092+iaminfinityiq@users.noreply.github.com>
2025-11-10 20:24:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ async def bot_loop():
 
                 data[str(member.id)]["infected_time"] = 0
                 data[str(member.id)]["infect_time"] = 0
-                await member.remove_roles(covid19_role)
+                await member.remove_roles(covid19)
 
         brainrot = discord.utils.get(member.roles, name="brainrot")
         if brainrot:
@@ -168,4 +168,5 @@ async def print_data(ctx):
             await ctx.send(str(json.load(file)))
 
 bot.run(token)
+
 
