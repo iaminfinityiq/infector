58c4b0ea13f153aa0ed7d5a2b744a4f126f1141b
iaminfinityiq <133767092+iaminfinityiq@users.noreply.github.com>
2025-11-10 18:46:47 +0700

    Fix formatting issues in main.py

--- main.py
+++ main.py
@@ -115,11 +115,13 @@ async def infect(ctx, infected: discord.Member):
     with open("data.json", "w") as file:
         json.dump(data, file)
 
+@bot.command()
 async def print_data(ctx):
     with open("data.json", "r") as file:
         await ctx.send(str(json.load(file)))
 
 bot.run(token)
+
 
 
 
