d2f273f5f11a59e64c6808a1990e5179d5f7a8e5
iaminfinityiq <133767092+iaminfinityiq@users.noreply.github.com>
2025-11-10 17:53:44 +0700

    Enhance bot with infection logic and status updates
    Added status update loop and role infection logic with time checks.

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ import logging # type: ignore
 from dotenv import load_dotenv
 import os # type: ignore
 from sys import exit # type: ignore
+import json # type: ignore
+from time import time # type: ignore
+from datetime import timedelta # type: ignore
 
 load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
+general = int(os.getenv("GENERAL"))
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -25,6 +29,20 @@ async def on_member_join(member):
     """
     When a member joins
     """
+
+@tasks.loop(seconds=1)
+async def status_update():
+    with open("data.json") as file:
+        data = json.load(file)
+        
+    for member in ctx.guilds.members:
+        covid19 = discord.utils.get(member.roles, name="covid 19")
+        if covid19 and int(time()) - data[member.id]["infected_time"] > 259200:
+            timeout_duration = timedelta(minutes=30)
+            await user.timeout(timeout_duration, reason="Infected with covid 19 for more than 3 days")
+            
+            channel = bot.get_channel(general)
+            await channel.send(f"User {member.mention} has been infected with covid 19 for more than 3 days. Because of that, {member.mention} will be timed out for 30 minutes!")
 
 @bot.event
 async def on_message(message):
@@ -46,6 +64,13 @@ async def on_message(message):
 
 @bot.command()
 async def infect(ctx, infected: discord.Member):
+    with open("data.json") as file:
+        data = json.load(file)
+
+    if int(time()) - data[infected]["infect_time"] < 86400:
+        await ctx.send("You have already infect someone within 24 hours, please wait for a moment before you can infect someone")
+        return
+    
     if infected.id == 1435931107521593344:
         await ctx.send("Cannot infect the Infector himself")
         return
@@ -57,8 +82,15 @@ async def infect(ctx, infected: discord.Member):
 
     covid19 = discord.utils.get(ctx.author.roles, name="covid 19")
     if covid19:
+        covid19 = discord.utils.get(infected.roles, name="covid 19")
+        if covid19:
+            await ctx.send(f"User {infected.mention} is already infected with covid 19")
+        
         await infected.add_roles(covid19)
         await ctx.send(f"Successfully infect user {infected.mention} using covid 19")
+            
+        data[infected.id]["infected_time"] = int(time.time())
+        data[ctx.author.id]["infect_time"] = int(time.time())
     else:
         await ctx.send(f"Cannot infect user {infected.mention} using covid 19 because you don't have that role")
     
@@ -66,7 +98,12 @@ async def infect(ctx, infected: discord.Member):
     if brainrot:
         await infected.add_roles(brainrot)
         await ctx.send(f"Successfully infect user {infected.mention} using brainrot")
+        data[infected.id]["infected_time"] = int(time.time())
+        data[ctx.author.id]["infect_time"] = int(time.time())
     else:
         await ctx.send(f"Cannot infect user {infected.mention} using brainrot because you don't have that role")
+
+    with open("data.json", "w") as file:
+        json.dump(data, file)
 
 bot.run(token)
