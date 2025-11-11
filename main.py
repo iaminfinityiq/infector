c301f9830419351a00e18342d654906dca2c50a5
iaminfinityiq <133767092+iaminfinityiq@users.noreply.github.com>
2025-11-11 18:57:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+FUCKING DIE
+
 import discord # type: ignore
 from discord.ext import commands, tasks # type: ignore
 import logging # type: ignore
@@ -165,6 +167,7 @@ async def print_data(ctx):
             await ctx.send(str(json.load(file)))
 
 bot.run(token)
+
 
 
 
